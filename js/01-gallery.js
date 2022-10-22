b7a21cf7c8361e25d3048b68ff5981df59b33171
Oleksiy07 <oleksij.komarnitskuj@gmail.com>
2022-10-22 14:39:31 +0300

    Створив галереї

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const images = document.querySelectorAll('.gallery__image');
+
+images.forEach(image => {
+    image.addEventListener('click', onImageClick);
+    
+    function onImageClick(e) {
+        e.preventDefault();
+        
+        const instance = basicLightbox.create(`
+        <img src="${image.dataset.source}" width="800" height="600">
+        `);
+        instance.show()
+    }
+});
+
+function createGalleryMarkup(images) {
+    return images
+    .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>
+        `;
+    })
+    .join('');
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const images = document.querySelectorAll('.gallery__image');
+
+images.forEach(image => {
+    image.addEventListener('click', onImageClick);
+    
+    function onImageClick(e) {
+        e.preventDefault();
+        
+        const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+    }
+});
+
+function createGalleryMarkup(images) {
+    return images
+    .map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    })
+    .join('');
+}
